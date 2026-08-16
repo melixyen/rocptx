c64b97a704b881274200a5f93f7b45a2ffb10f10
Melix <melixyen@gmail.com>
2026-08-16 23:45:07 +0800

    Add datatdx

--- dist/ptx.js
+++ dist/ptx.js
@@ -330,7 +330,7 @@
 
 	var defineProperty = _objectDp.f;
 	var _wksDefine = function (name) {
-	  var $Symbol = _core.Symbol || (_core.Symbol = _library ? {} : _global.Symbol || {});
+	  var $Symbol = _core.Symbol || (_core.Symbol = _global.Symbol || {});
 	  if (name.charAt(0) != '_' && !(name in $Symbol)) defineProperty($Symbol, name, { value: _wksExt.f(name) });
 	};
 
